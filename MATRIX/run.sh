572dc28b73387f9bfdc42d79b4c74369676f6b77
liorko87 <lior121987@gmail.com>
2020-03-18 19:39:28 +0200

    Add scripts for MATRIX integration

---
+++ MATRIX/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+./TwoThirdsHonestMajorityMalicious -partyID "${1}" -partiesNumber "${2}" -inputFile "${3}" -outputFile output.txt \
+-circuitFile "${4}" -fieldType "${5}" -partiesFile "${6}" -isHonest "${7}" -internalIterationsNumber "${8}" \
+-isPRF "${9}"
