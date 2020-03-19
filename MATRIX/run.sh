cfd475692bf1e0be9eb24827f41eb91e13f28680
liorko87 <lior121987@gmail.com>
2020-03-19 11:53:54 +0200

    Update scripts

--- MATRIX/run.sh
+++ MATRIX/run.sh
@@ -2,4 +2,4 @@
 
 ./TwoThirdsHonestMajorityMalicious -partyID "${1}" -partiesNumber "${2}" -inputFile "${3}" -outputFile output.txt \
 -circuitFile "${4}" -fieldType "${5}" -partiesFile "${6}" -isHonest "${7}" -internalIterationsNumber "${8}" \
--isPRF "${9}" -numberOfTriples "${10}"
+-isPRF "${9}" -numberOfTriples "${11}"
